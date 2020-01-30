bd75efb6c4c9b1ebc3a8148500ce23dd5a867b55
Guillamoure <jhilscheriv@gmail.com>
2020-01-30 14:33:23 -0500

    lession 16: ya yargs intro

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,45 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+yargs.version('1.1.0')
 
-if (command === 'add'){
-  console.log('adding note')
-} else if (command === 'remove'){
-  console.log('removing note')
-}
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function(){
+    console.log('adding a new note')
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function(){
+    console.log('removing a note')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Reading a note',
+  handler: function(){
+    console.log('reading a note')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function(){
+    console.log('Listing all notes')
+  }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
