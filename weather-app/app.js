9bd95b81dd893df24c4b8ee4780faea3bdcaed15
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-14 15:32:20 -0400

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,6 @@
 const request = require('postman-request')
 require('dotenv').config()
 
-
 const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK}&query=37.8267,-122.4233&units=f`
 
 request({ url: url, json: true }, (error, response) => {
