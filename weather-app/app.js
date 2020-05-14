3e683c3917b4b5f876f25cd795da228c8f6d2fae
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-14 15:10:21 -0400

    lession 32: customizing http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,8 @@
 const request = require('postman-request')
 
-const url = `http://api.weatherstack.com/current?access_key=a698ef9c949949b5a5b87c72a693b984&query=37.8267,-122.4233`
+const url = `http://api.weatherstack.com/current?access_key=a698ef9c949949b5a5b87c72a693b984&query=37.8267,-122.4233&units=f`
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-
-  console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+  let current = response.body.current
+  console.log(current.weather_descriptions[0] + `. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
 })
