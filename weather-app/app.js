f74b6b53451d9cb73b714793f623725ae54141a7
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-14 15:29:51 -0400

    dotenv and gitignore to hide api keys

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,8 @@
 const request = require('postman-request')
+require('dotenv').config()
 
-const url = `http://api.weatherstack.com/current?access_key=a698ef9c949949b5a5b87c72a693b984&query=37.8267,-122.4233&units=f`
+
+const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK}&query=37.8267,-122.4233&units=f`
 
 request({ url: url, json: true }, (error, response) => {
   let current = response.body.current
