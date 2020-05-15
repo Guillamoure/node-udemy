42efb976246e81c337bf2ea9b7cd6c251839a64c
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-14 22:55:55 -0400

    lesson 33: geocode http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,3 +7,12 @@ request({ url: url, json: true }, (error, response) => {
   let current = response.body.current
   console.log(current.weather_descriptions[0] + `. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
 })
+
+const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZ3VpbGxhbW91cmUiLCJhIjoiY2thNzZkZ2xyMDAyODJ4cDRqcGU5bHdpYyJ9.VHmdCno_w5xu3be1pV8R6Q"
+
+request({url: geocodeURL, json: true}, (error, response) => {
+  let location = response.body.features[0]
+  const latitude = location.center[1]
+  const longitude = location.center[0]
+  console.log(location.center)
+})
