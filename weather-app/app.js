c668826ec43db7eebd2a5b9b28cc9fbb90cc3c0c
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-15 10:46:23 -0400

    lesson 38: callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,12 +2,23 @@ require('dotenv').config()
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode("New York City", (error, data) => {
-  console.log("Error:", error)
-  console.log("Data:", data)
-})
+const input = process.argv[2]
 
-forecast(44.1545, -75.7068, (error, data) => {
-  console.log("Error:", error)
-  console.log(data.overall + `. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`)
+if(!input){
+  return console.log("Please provide a location")
+}
+
+geocode(input, (error, data) => {
+  if (error){
+    return console.log("Error:", error)
+  }
+  if (data){
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error){
+        return console.log("Error:", error)
+      }
+      console.log(data.location)
+      console.log(forecastData.overall + `. It is currently ${forecastData.temperature} degrees out. It feels like ${forecastData.feelslike} degrees out.`)
+    })
+  }
 })
