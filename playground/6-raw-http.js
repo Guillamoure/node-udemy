bd01cb4893f192a74e67914e79762947bd636222
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-16 15:06:53 -0400

    lesson 41: http request

---
+++ playground/6-raw-http.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const url = ``
+
+const request = http.request(url, (response) => {
+  let data = ''
+
+  response.on('data', (chunk) => {
+    data = data + chunk.toString()
+  })
+
+  response.on('end', () => {
+    const body = JSON.parse(data)
+    console.log(body)
+  })
+
+})
+
+request.on('error', (error) => {
+  console.log(error)
+})
+
+request.end()
