14f8d5a24fba2558cd41ac6b4984b6776a0b9926
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-16 18:24:07 -0400

    lesson 47: dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,30 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Guillamoure'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: "About Me",
+    name: "Guillamoure"
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: "Help",
+    message: "Oh god, everything is on fire!!!"
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
