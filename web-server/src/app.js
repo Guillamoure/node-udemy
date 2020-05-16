2ecb2792f2fcdb980c6e9b6ec6039554c8e3336d
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-16 17:54:27 -0400

    lesson 45: static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,9 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
-
-
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-  res.send({
-    name: "Guillamoure",
-    age: 21
-  })
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h2>About</h2>')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
   res.send({
