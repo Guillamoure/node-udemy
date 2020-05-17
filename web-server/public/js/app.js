a4509914616d71b3f7853e952fdf34fd4dbeb7cd
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-17 14:04:20 -0400

    lesson 57: client-side fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log("Client side javascript file is loaded.")
+
+fetch(`http://localhost:3000/weather?address=miani`)
+  .then(r => r.json())
+  .then(data => {
+    if (data.error){
+      console.log(data.error)
+    } else {
+      console.log(data)
+    }
+  })
