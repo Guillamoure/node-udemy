331bc6529fdb053703d75722db00be93e89f99fc
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-17 14:17:09 -0400

    lesson 58: search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,20 @@
 console.log("Client side javascript file is loaded.")
+const form = document.querySelector('form')
+const input = document.querySelector('input')
 
-fetch(`http://localhost:3000/weather?address=miani`)
-  .then(r => r.json())
-  .then(data => {
-    if (data.error){
-      console.log(data.error)
-    } else {
-      console.log(data)
-    }
-  })
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  runFetch(input.value)
+})
+
+const runFetch = (location) => {
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(r => r.json())
+    .then(data => {
+      if (data.error){
+        console.log(data.error)
+      } else {
+        console.log(data)
+      }
+    })
+}
