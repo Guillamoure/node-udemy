02775f13bb70f7f0790bf64018cbd8557eba25d9
Jack Hilscher <jhilscheriv@gmail.com>
2020-05-24 14:51:50 -0400

    lesson 79: promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,29 +1,41 @@
-setTimeout(() => {
-  console.log("Two seconds are up")
-}, 2000)
+// setTimeout(() => {
+//   console.log("Two seconds are up")
+// }, 2000)
+//
+// const names = ["Andrew", "Jen", "Jess"]
+// const shortNames = names.filter{(name) => {
+//   return name.length <= 4
+// }}
+//
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = { latitude: 0, longitude: 0 }
+//     callback(data)
+//   }, 2000)
+// }
+//
+// geocode('Philadelphia', (data) => {
+//   console.log(data)
+// })
+//
+// add(1, 4, (sum) => {
+//   console.log(sum)
+// })
+//
+// const add = (n1, n2, callback) => {
+//   setTimeout(() => {
+//     callback(n1+n2)
+//   }, 2000)
+// }
 
-const names = ["Andrew", "Jen", "Jess"]
-const shortNames = names.filter{(name) => {
-  return name.length <= 4
-}}
-
-const geocode = (address, callback) => {
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    const data = { latitude: 0, longitude: 0 }
-    callback(data)
+    // callback('This is my error', undefined)
+    callback(undefined, [1,4,7])
   }, 2000)
 }
 
-geocode('Philadelphia', (data) => {
-  console.log(data)
+doWorkCallback((error, result) => {
+  if(error){return console.log(error)}
+  console.log(result)
 })
-
-add(1, 4, (sum) => {
-  console.log(sum)
-})
-
-const add = (n1, n2, callback) => {
-  setTimeout(() => {
-    callback(n1+n2)
-  }, 2000)
-}
